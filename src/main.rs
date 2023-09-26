ff347d4879620b690735c707feac808570264642
Kyle Zhang <kylezzhang@berkeley.edu>
2023-09-26 13:31:26 -0700

    12.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        panic!("Needs 3 args");
+    }
+
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {} in file {}", query, file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("With text:\n {contents}");
 }
