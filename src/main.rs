2c964b0eba79ce67e9db2b48038d5411568b84a8
Kyle Zhang <kylezzhang@berkeley.edu>
2023-09-27 22:27:00 -0700

    12.3

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::env;
-use std::fs;
 
+use minigrep::Config;
+use std::env;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
-        panic!("Needs 3 args");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("Searching for {} in file {}", config.query, config.file_path);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
-
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {} in file {}", query, file_path);
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    println!("With text:\n {contents}");
 }
